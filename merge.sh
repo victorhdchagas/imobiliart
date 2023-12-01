06d7b4890072cba1a93aa0a7313a8fb83d29d735
victorhdchagas <victorhdchagas@live.com>
2023-12-01 00:01:27 -0300

    Creating merge script

---
+++ merge.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+
+git checkout main #switch to main branch
+
+git pull origin main # grab latest updates from main branch
+
+git merge dev # merge dev branch to your main branch
+
+git push origin main # push your changes to main
